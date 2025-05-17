fa233bb7efadeae9437b41c6cb94457aef223343
kuwamin <kuwamin1119@gmail.com>
2025-05-18 02:32:39 +0900

    投手が選択できるように改良_6

--- app.py
+++ app.py
@@ -28,42 +28,6 @@ def generate_random_stats():
 def index():
     return render_template('index.html')
 
-@app.route('/register_pitcher', methods=['GET', 'POST'])
-def register_pitcher():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        pitch_speed = int(request.form.get('pitch_speed'))
-        control = int(request.form.get('control'))
-        stamina = int(request.form.get('stamina'))
-        breaking_ball = int(request.form.get('breaking_ball'))
-
-        existing = PlayerModel.query.filter_by(name=name).first()
-        if existing:
-            return render_template("register_pitcher.html", message=f"{name} はすでに登録されています。")
-
-        player = PlayerModel(
-            name=name,
-            trajectory=2,
-            contact=0,
-            power=0,
-            speed=0,
-            arm=0,
-            defense=0,
-            catch=0,
-            is_pitcher=True,
-            pitch_speed=pitch_speed,
-            control=control,
-            stamina=stamina,
-            breaking_ball=breaking_ball
-        )
-
-        db.session.add(player)
-        db.session.commit()
-        return render_template("register_pitcher.html", message=f"{name} を登録しました！")
-
-    return render_template("register_pitcher.html")
-
-
 
 @app.route('/players')
 def show_players():
@@ -302,13 +266,21 @@ def register_pitcher():
             pitch_speed=pitch_speed,
             control=control,
             stamina=stamina,
-            breaking_ball=breaking_ball
+            breaking_ball=breaking_ball,
+            trajectory=2,
+            contact=0,
+            power=0,
+            speed=0,
+            arm=0,
+            defense=0,
+            catch=0
         )
         db.session.add(player)
         db.session.commit()
         return render_template("register_pitcher.html", message=f"{name} を投手として登録しました！")
 
     return render_template("register_pitcher.html")
+
 
 
 if __name__ == '__main__':
